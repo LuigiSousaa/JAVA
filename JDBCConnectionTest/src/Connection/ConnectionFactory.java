c1571c665072043982b928b3d44d1b97134049b9
Luigi Sousa <luigienricossilva@gmail.com>
2023-10-31 16:58:24 -0300

    update

---
+++ JDBCConnectionTest/src/Connection/ConnectionFactory.java
@@ -0,0 +1,42 @@
+package Connection;
+
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USUARIO = "postgres";
+    private static final String SENHA = "postgres";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USUARIO, SENHA);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao obter conexão com o banco de dados.", e);
+        }
+    }
+
+    public static void closeConnection(Connection connection) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void closeConnection(Connection connection, PreparedStatement stmt) {
+        try {
+            if (connection != null && stmt != null) {
+                connection.close();
+                stmt.close();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
